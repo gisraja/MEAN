14908c58b2a83dd394b797141d119540df78386a
Raja Narayanan <rn077c@att.com>
2019-01-02 12:34:41 -0600

    register changes

---
+++ ngApp/src/app/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private _registerUrl = "http://localhost:3000/api/register"
+  constructor(private http: HttpClient) {
+
+   }
+
+   private registerUser(user){
+     return this.http.post<any>(this._registerUrl, user);
+
+   }
+}
